0c136e0708e6824fb1b3ee4cac889a809c38b3b1
hrushie <hrushie@gmail.com>
2017-06-21 14:35:35 -0700

    Second one

---
+++ app/src/main/java/com/hrushie/rushikeshjawali/MessagesFragment.java
@@ -0,0 +1,46 @@
+package com.hrushie.rushikeshjawali;
+
+/**
+ * Created by hrushie on 6/21/2017.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MessagesFragment extends Fragment {
+
+    public MessagesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_messages, container, false);
+
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
